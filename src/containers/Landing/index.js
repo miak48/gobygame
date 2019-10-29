fea50ea840b8da9798e5a99bae6636eb21a640e0
mia kent <miak48@gmail.com>
2019-10-29 18:03:20 +1100

    I tried to make the home page pretty but I can't figure out why 100vw isn't working...

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+import goby_home from '../../assets/images/goby_home.png';
 import background from '../../assets/images/gravel2.png';
 import styles from './Landing.module.scss';
 
 function Landing() {
   return (
-    <div className={styles.Landing}>
-      <h1>Hello, Mia! We ❤️  you</h1>
-      <h3>This page is intentionally ugly, we need you to clean it up!</h3>
-      <h6>I know you are up to the task</h6>
-      <Link to='/game'>Link to the Game page</Link>
+    <div class="backgroundgoby">
+      <img src={goby_home} alt="" />
+      <h1>Goby Game </h1>
+      <h4>a description of the game to go here</h4>
     </div>
   );
 }
