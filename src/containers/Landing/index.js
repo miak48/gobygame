09b0b4ff31f5c9373beb6a45e312be1ae716c992
James Kent <jkent@chariotsolutions.com>
2019-11-11 21:34:38 -0500

    Cleaning up landing page

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import goby_home from '../../assets/images/goby_home.png';
 import styles from './Landing.module.scss';
 
-function Landing() {
+const Landing = () => {
   return (
-    <div class="overallstyle">
-      <img src={goby_home} alt=" " className={styles.backgroundgoby} />
-      <h1>Goby Game</h1>
-      <h4>a description of how to play the game </h4>
-      <Link to="/Demo"><button className={styles.button}>View Demo</button></Link>     
+    <div className={styles.Background}>
+      <div className={styles.Title}>
+        <h1>Goby Game</h1>
+        <h4>An awesome description of how to play this fun game!</h4>
+      </div>
+      <Link to="/Demo">
+        <button className={styles.CustomButton}>
+          View Demo
+        </button>
+      </Link>
     </div>
   );
-}
+};
 
 export default Landing;
